64edb2fcd54ca36bee6db789f4eeed3c4b2a4b40
vince <vincent@djengo.net>
2012-06-12 17:39:01 +0200

    initial_point

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogForDjengo::Application.config.secret_token = 'c9d5e482634df658bd3b5d1ac26a06cd5b8cc49eaf115585e9011dbd6b3ff85a2312a6ccd586881a92cd6bbc2e94e1cabf0e85eaf745aa2bf0122af6eecff709'
